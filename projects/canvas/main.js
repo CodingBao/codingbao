5de8ce67ab1de18c430130a946b28ba0f57f4262
niceTang1201 <2088412617@qq.com>
2018-12-04 23:11:44 +0800

    canvas

--- projects/canvas/main.js
+++ projects/canvas/main.js
@@ -14,42 +14,121 @@ function onInitCanvas() {
     }
 }
 
-context.fillStyle = "rgb(255,0,0)"
-
-//画直线
-function drawLine(srcX,srcY,dstX,dstY){
-    context.beginPath()
-    context.moveTo(srcX,srcY)
-    context.lineTo(dstX,dstY)
-    context.stroke()
-}
-
-var bInUse = false
 var bInEraser = false
-var oldPoint = {x:undefined,y:undefined}
-window.onmousedown = function(a){
-    bInUse = true
-    oldPoint = {x:a.clientX,y:a.clientY}
+var eraserBtn = document.getElementById("eraser")
+var penBtn = document.getElementById("pen")
+var clearBtn = document.getElementById("clear")
+eraserBtn.onclick = function () {
+    bInEraser = true
+    eraserBtn.classList.add("actived")
+    penBtn.classList.remove("actived")
 }
 
-window.onmousemove = function(a){
-    if(bInUse){
-        if(bInEraser){
-            context.clearRect(a.clientX-5,a.clientY-5,10,10)
-        }else{
-            var newPoint = {x:a.clientX,y:a.clientY}
-            drawLine(oldPoint.x,oldPoint.y,newPoint.x,newPoint.y)
-            oldPoint = newPoint
-        }
+penBtn.onclick = function () {
+    bInEraser = false
+    eraserBtn.classList.remove("actived")
+    penBtn.classList.add("actived")
+}
+
+clearBtn.onclick = function () {
+    context.clearRect(0,0,document.documentElement.clientWidth,document.documentElement.clientHeight)
+}
+
+red.onclick = function(){
+    context.strokeStyle = "red"
+}
+green.onclick = function(){
+    context.strokeStyle = "green"
+}
+blue.onclick = function(){
+    context.strokeStyle = "blue"
+}
+
+document.getElementById("lineWidthNum").value = 5
+var lineWidth = document.getElementById("lineWidthNum").value
+var addBtn = document.getElementById("add")
+var reduceBtn = document.getElementById("reduce")
+addBtn.onclick = function(){
+    document.getElementById("lineWidthNum").value++
+}
+reduceBtn.onclick = function(){
+    if(document.getElementById("lineWidthNum").value > 1){
+        document.getElementById("lineWidthNum").value--
     }
 }
 
-window.onmouseup = function(a){
-    bInUse = false
+
+var bInUse = false
+var oldPoint = {
+    x: undefined,
+    y: undefined
+}
+draw()
+//画直线
+function drawLine(srcX, srcY, dstX, dstY) {
+    context.beginPath()
+    context.lineWidth = lineWidth
+    context.moveTo(srcX, srcY)
+    context.lineTo(dstX, dstY)
+    context.stroke()
 }
 
-var eraserBtn = document.getElementById("eraser")
-eraserBtn.onclick = function(){
-    bInEraser = !bInEraser
-}
+function draw() {
+    if (window.ontouchstart == undefined) {
+        window.onmousedown = function (a) {
+            lineWidth = document.getElementById("lineWidthNum").value
+            bInUse = true
+            oldPoint = {
+                x: a.clientX,
+                y: a.clientY
+            }
+        }
 
+        window.onmousemove = function (a) {
+            if (bInUse) {
+                if (bInEraser) {
+                    context.clearRect(a.clientX - lineWidth/2, a.clientY - lineWidth/2, lineWidth, lineWidth)
+                } else {
+                    var newPoint = {
+                        x: a.clientX,
+                        y: a.clientY
+                    }
+                    drawLine(oldPoint.x, oldPoint.y, newPoint.x, newPoint.y)
+                    oldPoint = newPoint
+                }
+            }
+        }
+
+        window.onmouseup = function (a) {
+            bInUse = false
+        }
+    } else {
+        window.ontouchstart = function (a) {
+            lineWidth = document.getElementById("lineWidthNum").value
+            bInUse = true
+            oldPoint = {
+                x: a.touches[0].clientX,
+                y: a.touches[0].clientY
+            }
+        }
+
+        window.onmousemove = function (a) {
+            if (bInUse) {
+                if (bInEraser) {
+                    context.clearRect(a.touches[0].clientX - lineWidth/2, a.touches[0].clientY - lineWidth/2, lineWidth, lineWidth)
+                } else {
+                    var newPoint = {
+                        x: a.touches[0].clientX,
+                        y: a.touches[0].clientY
+                    }
+                    drawLine(oldPoint.x, oldPoint.y, newPoint.x, newPoint.y)
+                    oldPoint = newPoint
+                }
+            }
+        }
+
+        window.onmouseup = function (a) {
+            bInUse = false
+        }
+    }
+}
